e08c26a70d136b46ff230e9bd15327ba5a2e7992
mertfindicak <mfindicak@gmail.com>
2022-03-21 01:58:36 +0300

    Added file upload.

--- renderer.js
+++ renderer.js
@@ -1,6 +1,8 @@
 var dbx = new Dropbox.Dropbox({
-  accessToken: token
+  accessToken: token,
 });
+
+var currentPath = '/';
 
 const addElementToList = (listId, element) => {
   var ul = document.getElementById(listId);
@@ -18,7 +20,7 @@ const addElementToList = (listId, element) => {
     downloadButton.addEventListener('click', () =>
       downloadFolder(element.path_display, element.name)
     );
-  downloadButton.innerText = 'İndir';
+  downloadButton.innerText = 'Download';
   downloadButton.style = 'margin-left:10px';
   div.style = 'display:flex;margin:5px';
   div.appendChild(downloadButton);
@@ -37,6 +39,92 @@ const downloadFolder = (path, fileName) => {
   });
 };
 
+function uploadFile() {
+  var path = '/';
+  if (currentPath && currentPath !== '') path = currentPath;
+  const UPLOAD_FILE_SIZE_LIMIT = 150 * 1024 * 1024;
+  var fileInput = document.getElementById('file-upload');
+  var file = fileInput.files[0];
+
+  if (file.size < UPLOAD_FILE_SIZE_LIMIT) {
+    // File is smaller than 150 Mb - use filesUpload API
+    dbx
+      .filesUpload({ path: path + file.name, contents: file })
+      .then(function (response) {
+        var results = document.getElementById('results');
+        var br = document.createElement('br');
+        results.appendChild(document.createTextNode('File uploaded!'));
+        results.appendChild(br);
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  } else {
+    // File is bigger than 150 Mb - use filesUploadSession* API
+    const maxBlob = 8 * 1000 * 1000; // 8Mb - Dropbox JavaScript API suggested max file / chunk size
+
+    var workItems = [];
+
+    var offset = 0;
+
+    while (offset < file.size) {
+      var chunkSize = Math.min(maxBlob, file.size - offset);
+      workItems.push(file.slice(offset, offset + chunkSize));
+      offset += chunkSize;
+    }
+
+    const task = workItems.reduce((acc, blob, idx, items) => {
+      if (idx == 0) {
+        // Starting multipart upload of file
+        return acc.then(function () {
+          return dbx
+            .filesUploadSessionStart({ close: false, contents: blob })
+            .then((response) => response.session_id);
+        });
+      } else if (idx < items.length - 1) {
+        // Append part to the upload session
+        return acc.then(function (sessionId) {
+          var cursor = { session_id: sessionId, offset: idx * maxBlob };
+          return dbx
+            .filesUploadSessionAppendV2({
+              cursor: cursor,
+              close: false,
+              contents: blob,
+            })
+            .then(() => sessionId);
+        });
+      } else {
+        // Last chunk of data, close session
+        return acc.then(function (sessionId) {
+          var cursor = { session_id: sessionId, offset: file.size - blob.size };
+          var commit = {
+            path: '/' + file.name,
+            mode: 'add',
+            autorename: true,
+            mute: false,
+          };
+          return dbx.filesUploadSessionFinish({
+            cursor: cursor,
+            commit: commit,
+            contents: blob,
+          });
+        });
+      }
+    }, Promise.resolve());
+
+    task
+      .then(function (result) {
+        var results = document.getElementById('results');
+        results.appendChild(document.createTextNode('File uploaded!'));
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+  return false;
+}
+
 const clearTheList = (listId) => {
   const ourList = document.getElementById(listId);
   ourList.innerHTML = '';
@@ -44,6 +132,7 @@ const clearTheList = (listId) => {
 
 const getFileList = (listId, path) => {
   clearTheList(listId);
+  currentPath = path + '/';
   dbx
     .filesListFolder({ path: path })
     .then(function (response) {
