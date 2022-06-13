0268290e527d33e76aa53acf9bcc6b7c0b97c9bc
mertfindicak <mfindicak@gmail.com>
2022-06-13 21:47:56 +0300

    Added firebase connection.

---
+++ firebaseData.js
@@ -0,0 +1,35 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js';
+import {
+  getFirestore,
+  collection,
+  addDoc,
+} from 'https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js';
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  apiKey: 'AIzaSyC349s0GI1RnTnJvwLC1E7fQ39Yr8a5k-4',
+  authDomain: 'krallaragguvenligi-21ef0.firebaseapp.com',
+  projectId: 'krallaragguvenligi-21ef0',
+  storageBucket: 'krallaragguvenligi-21ef0.appspot.com',
+  messagingSenderId: '122724089734',
+  appId: '1:122724089734:web:309bc585cbdf52f5fab607',
+  measurementId: 'G-Y9N2632M1Z',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+try {
+  const docRef = await addDoc(collection(db, 'users'), {
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815,
+  });
+  console.log('Document written with ID: ', docRef.id);
+} catch (e) {
+  console.error('Error adding document: ', e);
+}
