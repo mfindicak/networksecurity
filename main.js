8cf71c424d55ea3f1cae74838fcbb4a67a7bfbc3
mertfindicak <mfindicak@gmail.com>
2022-06-14 00:16:36 +0300

    Added getPrivateKey and getPublicKey functions.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const fs = require('fs');
+var crypto = require('crypto');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -29,10 +30,106 @@ async function createWindow() {
 app.on('ready', createWindow);
 
 ipcMain.on('toMain', (event, args) => {
-  fs.readFile(args, (error, data) => {
-    // Do something with file contents
+  switch (args.function) {
+    case 'readFile':
+      fs.readFile(args.fileName, 'utf8', (error, data) => {
+        // Do something with file contents
 
-    // Send result back to renderer process
-    win.webContents.send('fromMain', data);
-  });
+        // Send result back to renderer process
+        win.webContents.send('fromMain', {
+          function: 'readFile',
+          data: data,
+        });
+      });
+      break;
+    case 'getPublicKey':
+      fs.readFile('publicKey.pem', 'utf8', (error, data) => {
+        // Do something with file contents
+
+        // Send result back to renderer process
+        win.webContents.send('fromMain', {
+          function: 'getPublicKey',
+          data: data,
+        });
+      });
+      break;
+    case 'getPrivateKey':
+      fs.readFile('privateKey.pem', 'utf8', (error, data) => {
+        // Do something with file contents
+
+        // Send result back to renderer process
+        win.webContents.send('fromMain', {
+          function: 'getPrivateKey',
+          data: data,
+        });
+      });
+      break;
+    default:
+  }
 });
+
+//Kriptolama
+
+const generateKeys = () => {
+  crypto.generateKeyPair(
+    'rsa',
+    {
+      modulusLength: 4096,
+      publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem',
+      },
+      privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+        cipher: 'aes-256-cbc',
+        passphrase: 'top secret',
+      },
+    },
+    (err, publicKey, privateKey) => {
+      // Handle errors and use the generated key pair.
+      fs.writeFileSync('privateKey.pem', privateKey);
+      fs.writeFileSync('publicKey.pem', publicKey);
+      //   const sifreli = encryptWithPublicKey('Ufuk harikasın');
+      //   console.log('Sifreli:' + sifreli);
+      //   const cozuldu = decryptWithPrivateKey(sifreli);
+      //   console.log('Cozuldu:' + cozuldu);
+    }
+  );
+};
+
+try {
+  if (fs.existsSync('./publicKey.pem') && fs.existsSync('./privateKey.pem')) {
+    console.log('Public ve Private Key Kayıtlı');
+  } else {
+    generateKeys();
+  }
+} catch (err) {
+  generateKeys();
+}
+
+const encryptWithPublicKey = (plainText) => {
+  return crypto.publicEncrypt(
+    {
+      key: fs.readFileSync('publicKey.pem', 'utf8'),
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: 'sha256',
+    },
+    // We convert the data string to a buffer
+    Buffer.from(plainText)
+  );
+};
+
+const decryptWithPrivateKey = (encryptedText) => {
+  return crypto.privateDecrypt(
+    {
+      key: fs.readFileSync('privateKey.pem', 'utf8'),
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: 'sha256',
+      passphrase: 'top secret',
+    },
+    encryptedText
+  );
+};
+
+//Kriptolama
